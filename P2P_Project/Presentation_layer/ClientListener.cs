c6bafed01250e8f485cfde8ae8a4f1d1079aa311
Ondřej Krejčí <ondra.krejci13@gmail.com>
2026-01-23 17:31:31 +0100

    Created basic structure of Presentation layer

---
+++ P2P_Project/Presentation_layer/ClientListener.cs
@@ -0,0 +1,82 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Windows.Controls;
+
+namespace P2P_Project.Presentation_layer
+{
+    public class ClientListener
+    {
+        private TcpListener _listener;
+        private bool _isRunning;
+        private Thread _clientAcceptor;
+        private List<ConnectionManager> _clients;
+
+        private string _ipAddress;
+        private int _port;
+        private int _timeoutTime;
+
+        private StackPanel _errorPanel;
+
+        public ClientListener(string ipAddress, int port, int _timeoutTime, StackPanel errorPanel)
+        {
+            _listener = new TcpListener(IPAddress.Parse(ipAddress), port);
+            _clients = new List<ConnectionManager>();
+            _ipAddress = ipAddress;
+            _port = port;
+            this._timeoutTime = _timeoutTime;
+
+            _errorPanel = errorPanel;
+
+
+        }
+
+
+        private void AcceptClient()
+        {
+            while (_isRunning)
+            {
+                try
+                {
+                    TcpClient client = _listener.AcceptTcpClient();
+                    ConnectionManager connectionManager = new ConnectionManager(client,_errorPanel);
+                    _clients.Add(connectionManager);
+                    connectionManager.Run();
+                }
+                catch (SocketException socketEx)
+                {
+                    ErrorLog("Socket", socketEx.Message);
+                }
+                catch (Exception ex)
+                {
+                    ErrorLog("General", ex.Message);
+                }
+
+            }
+        }
+
+
+        private static string GetLocalIPAddress()
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            var ipAddress = host.AddressList
+                .FirstOrDefault(ip => ip.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork);
+
+            if (ipAddress == null)
+                throw new Exception("No IPv4 address found for this machine.");
+
+            return ipAddress.ToString();
+        }
+
+        private void ErrorLog(string erronName, string errorMessage)
+        {
+            TextBlock errorText = new TextBlock
+            {
+                Text = $"{erronName} error (Client Listener): {errorMessage}",
+                Foreground = System.Windows.Media.Brushes.Red,
+                Margin = new System.Windows.Thickness(5)
+            };
+            _errorPanel.Children.Add(errorText);
+        }
+
+    }
+}
---
+++ P2P_Project/Presentation_layer/ConnectionManager.cs
@@ -0,0 +1,83 @@
+﻿using System.IO;
+using System.Net.Sockets;
+using System.Windows.Controls;
+
+namespace P2P_Project.Presentation_layer
+{
+    public class ConnectionManager
+    {
+        private TcpClient _client;
+        private Thread _clientThread;
+        private bool _isRunning;
+
+        private StreamReader _reader;
+        private StreamWriter _writer;
+
+        private StackPanel _errorPanel;
+
+        private CommandParser _commandParser;
+
+        public ConnectionManager(TcpClient client, StackPanel errorPanel)
+        {
+            _commandParser = new CommandParser();
+            _client = client;
+            _reader = new StreamReader(_client.GetStream());
+            _writer = new StreamWriter(_client.GetStream()) { AutoFlush = true };
+        }
+
+
+        public void Run()
+        {
+            try
+            {
+                do
+                {
+                    Do();
+                } 
+                while (_isRunning);
+            }
+            catch (Exception ex)
+            {
+                TextBlock errorText = new TextBlock
+                {
+                    Text = $"Connection error: {ex.Message}",
+                    Foreground = System.Windows.Media.Brushes.Red,
+                    Margin = new System.Windows.Thickness(5)
+                };
+                _errorPanel.Children.Add(errorText);
+            }
+        }
+
+        private void Do()
+        {
+            try
+            {
+                string clientInput = _reader.ReadLine();
+
+                string[] parsedCommand = _commandParser.Parse(clientInput);
+
+                // odeslani na command executor - parsnuty komand a tcpclient
+
+            }
+            catch (Exception ex)
+            {
+                TextBlock errorText = new TextBlock
+                {
+                    Text = $"Communication error (Connection Manager): {ex.Message}",
+                    Foreground = System.Windows.Media.Brushes.Red,
+                    Margin = new System.Windows.Thickness(5)
+                };
+                _errorPanel.Children.Add(errorText);
+            }
+
+        }
+
+        private void Stop()
+        {
+            _isRunning = false;
+            _client.Close();
+            _clientThread.Join();
+        }
+
+    }
+}
