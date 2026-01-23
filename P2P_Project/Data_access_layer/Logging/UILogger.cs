a2c26f9997bf911443bb248b0b21c34743f49894
N4MERR <namertyzhx@gmail.com>
2026-01-23 18:25:28 +0100

    feat: Add Logger

---
+++ P2P_Project/Data_access_layer/Logging/UILogger.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2P_Project.Data_access_layer.Logging
+{
+    public class UILogger : ILogger
+    {
+        private readonly Action<string> _updateUiAction;
+
+        public UILogger(Action<string> updateUiAction) => _updateUiAction = updateUiAction;
+
+        public void Log(string message)
+        {
+            _updateUiAction.Invoke($"STATUS: {message}");
+        }
+    }
+}
