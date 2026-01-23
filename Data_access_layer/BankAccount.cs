8066fc9a8709e53e7ad32ad2a3968fb8af439243
N4MERR <namertyzhx@gmail.com>
2026-01-23 16:39:08 +0100

    feat: Add class for bank accounts

---
+++ Data_access_layer/BankAccount.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+public class BankAccount
+{
+    private int _accountNumber;
+    private float _balance;
+
+    public BankAccount(int accountNumber, float balance)
+    {
+        AccountNumber = accountNumber;
+        Balance = balance;
+    }
+
+    public int AccountNumber
+    {
+        get => _accountNumber;
+        set
+        {
+            if (value < 10000 || value > 99999)
+            {
+                throw new ArgumentException("Account number must be between 10000 and 99999.");
+            }
+            _accountNumber = value;
+        }
+    }
+
+    public float Balance
+    {
+        get => _balance;
+        set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Balance cannot be negative.");
+            }
+            _balance = value;
+        }
+    }
+}
