665e7f9da6a974fbfbda4cace8ae9093a8efc480
N4MERR <namertyzhx@gmail.com>
2026-01-24 12:17:53 +0100

    feat: Add logging using Serilog library
    Changed logging to use Serilog library and deleted old logger.

--- P2P_Project/MainWindow.xaml.cs
+++ P2P_Project/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using P2P_Project.Data_access_layer;
 using P2P_Project.Presentation_layer;
+using Serilog;
 using System.Windows;
 
 
@@ -13,13 +14,31 @@ namespace P2P_Project
         private ClientListener _clientListener;
         private ConfigLoader _configLoader;
 
+        private void CreateLogger()
+        {
+            Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Debug()
+            .WriteTo.File(
+                path: "logs/myapp.txt",
+                rollingInterval: RollingInterval.Day,
+                flushToDiskInterval: TimeSpan.FromSeconds(1),
+                shared: true
+            )
+            //.WriteTo.Observers(events => events
+            //    .Do(evt =>
+            //    {
+            //    }).Subscribe())
+            .CreateLogger();
+        }
+
         public MainWindow()
         {
+            CreateLogger();
+
             InitializeComponent();
             _configLoader = new ConfigLoader();
 
-
-            _clientListener = new ClientListener("127.0.0.1",8000,50, ErrorPanel); //(_configLoader.IPAddress,_configLoader.Port, _configLoader.TimeoutTime, Errors);
+            _clientListener = new ClientListener("127.0.0.1", 8000, 50, ErrorPanel); //(_configLoader.IPAddress,_configLoader.Port, _configLoader.TimeoutTime, Errors);
             _clientListener.Start();
         }
     }
