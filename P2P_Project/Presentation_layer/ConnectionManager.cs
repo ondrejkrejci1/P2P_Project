f2d860b9aebeba4d4b3248c5e2f3d30f406e875d
N4MERR <namertyzhx@gmail.com>
2026-01-24 17:55:23 +0100

    feat: Add ConnectionManager with function to send message to clients

--- P2P_Project/Presentation_layer/ConnectionManager.cs
+++ P2P_Project/Presentation_layer/ConnectionManager.cs
@@ -1,108 +1,37 @@
-﻿using P2P_Project.Application_layer;
-using System.IO;
+﻿using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Net.Sockets;
-using System.Windows;
-using System.Windows.Controls;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace P2P_Project.Presentation_layer
 {
     public class ConnectionManager
     {
-        private TcpClient _client;
-        private Thread _clientThread;
-        private bool _isRunning;
+        private static readonly ConnectionManager _instance = new ConnectionManager();
 
-        private StreamReader _reader;
-        private StreamWriter _writer;
+        public static ConnectionManager Instance => _instance;
 
-        private StackPanel _errorPanel;
-        private StackPanel _clientPanel;
+        public List<TcpConnection> Connections { get; set; }
 
-        private CommandParser _commandParser;
-        private CommandExecutor _commandExecutor;
-
-        public ConnectionManager(TcpClient client, StackPanel errorPanel, StackPanel clientPanel)
+        private ConnectionManager()
         {
-            _commandParser = new CommandParser();
-            _commandExecutor = new CommandExecutor();
-            _client = client;
-            _errorPanel = errorPanel;
-            _reader = new StreamReader(_client.GetStream());
-            _writer = new StreamWriter(_client.GetStream()) { AutoFlush = true };
-            _isRunning = true;
-            _clientPanel = clientPanel;
+            Connections = new List<TcpConnection>();
         }
 
-
-        public void Run()
+        public void SendMessage(TcpClient client, string message)
         {
             try
             {
-                do
-                {
-                    Do();
-                } 
-                while (_isRunning);
+                byte[] data = Encoding.UTF8.GetBytes(message + Environment.NewLine);
+                client.GetStream().Write(data, 0, data.Length);
             }
             catch (Exception ex)
             {
-                ErrorLog("RunLoop", ex.Message);
+                Log.Error($"Failed to send message({message}) to client: {ex.Message}");    
             }
         }
-
-        private void Do()
-        {
-            try
-            {
-                string clientInput = _reader.ReadLine();
-
-                string[] parsedCommand = _commandParser.Parse(clientInput);
-
-                _commandExecutor.ExecuteCommand(_client, parsedCommand);
-
-            }
-            catch (IOException)
-            {
-                if (!_isRunning)
-                {
-                    return;
-                }
-
-                ErrorLog("Communication", "Connection unexpectedly terminated.");
-                _isRunning = false;
-            }
-            catch (Exception ex)
-            {
-                ErrorLog("Communication", ex.Message);
-            }
-
-        }
-
-        public void Stop()
-        {
-            // poslani erroru uzivatelum o necekane chybe serveru
-
-            _isRunning = false;
-            if (_clientThread != null )
-                _clientThread.Join(1000);
-            _client.Close();
-        }
-
-        private void ErrorLog(string erronName, string errorMessage)
-        {
-            Application.Current.Dispatcher.Invoke(() =>
-            {
-                TextBlock errorText = new TextBlock
-                {
-                    Text = $"{erronName} error (Client Listener): {errorMessage}",
-                    Foreground = System.Windows.Media.Brushes.Red,
-                    Margin = new Thickness(10,5,0,0)
-
-                };
-                _errorPanel.Children.Add(errorText);
-            });
-        }
-
-
     }
 }
